982a99b2d7b4b9d614421774ebc8f04b16af571a
luis.nunes <luis.nunes@mail.uft.edu.br>
2022-09-08 22:27:51 -0300

    memo | useMemo

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 interface IProductItemProps {
     product: {
         id: number;
@@ -6,10 +8,23 @@ interface IProductItemProps {
     }
 }
 
-export function ProductItem({product}: IProductItemProps) {
+function ProductComponent({product}: IProductItemProps) {
     return (
         <div>
             {product.title} - <strong>{product.price}</strong>
         </div>
     )
 }
+
+/**
+ * O memo via fazer uma comparação rasa entre os dois para evitar uma nova renderização
+ * Usa apenas em components que será renderizado mais de uma vez na aplicação
+ * Em components limpos
+ * e components que irão não vai mudar a propriedade constantemente
+ */
+
+export const ProductItem = memo(ProductComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product)
+})
+
+// export const ProductItem = ProductComponent
--- src/components/SearchProducts.tsx
+++ src/components/SearchProducts.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
 interface ISearchProductsProps {
@@ -9,8 +10,19 @@ interface ISearchProductsProps {
 }
 
 export function SearchProducts({results}: ISearchProductsProps) {
+    const totalPrice = useMemo(() => {
+        return results.reduce((total, product) => {
+            return total + product.price
+        }, 0)
+    }, [results])
+
+
+
     return (
         <div>
+
+        <h2>{ totalPrice }</h2>
+
         {results && results.map(item => (
             <li key={item.id}>
                 <ProductItem product={item} />
